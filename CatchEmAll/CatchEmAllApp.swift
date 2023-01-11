d6815eb6c7d8d63bcf82fca23371bcca55461a98
Dmitry Sharabin <dmitrysharabin@gmail.com>
2023-01-11 11:45:51 +0100

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Dmitry Sharabin on 11.1.23..
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
