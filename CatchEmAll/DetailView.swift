d1c75bc458a335bc7e26e90c7d28cdb167ae139b
Dmitry Sharabin <dmitrysharabin@gmail.com>
2023-01-11 13:35:56 +0100

    Ch. 6.5

--- CatchEmAll/DetailView.swift
+++ CatchEmAll/DetailView.swift
@@ -25,18 +25,37 @@ struct DetailView: View {
                 .padding(.bottom)
             
             HStack {
-                Image(systemName: "figure.run.circle")
-                    .resizable()
-                    .scaledToFit()
-                    .backgroundStyle(.white)
-                    .frame(maxHeight: 96)
-                    .cornerRadius(16)
-                    .shadow(radius: 8, x: 5, y: 5)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 16)
-                            .stroke(.gray.opacity(0.5), lineWidth: 1)
-                    )
-                    .padding(.trailing)
+                AsyncImage(url: URL(string: creatureDetailVM.imageURL)) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .background(.white)
+                        .frame(maxHeight: 96)
+                        .cornerRadius(16)
+                        .shadow(radius: 8, x: 5, y: 5)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 16)
+                                .stroke(.gray.opacity(0.5), lineWidth: 1)
+                        )
+                        .padding(.trailing)
+                } placeholder: {
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundColor(.clear)
+                        .frame(maxWidth: 96, maxHeight: 96)
+                }
+
+//                Image(systemName: "figure.run.circle")
+//                    .resizable()
+//                    .scaledToFit()
+//                    .backgroundStyle(.white)
+//                    .frame(maxHeight: 96)
+//                    .cornerRadius(16)
+//                    .shadow(radius: 8, x: 5, y: 5)
+//                    .overlay(
+//                        RoundedRectangle(cornerRadius: 16)
+//                            .stroke(.gray.opacity(0.5), lineWidth: 1)
+//                    )
+//                    .padding(.trailing)
                 
                 VStack(alignment: .leading) {
                     HStack(alignment: .top) {
